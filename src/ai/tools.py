f746a1add6a2470b5cd7cc7d4dfb3f0ac7226c8f
Ashish RV <ashish14141@gmail.com>
2025-06-29 19:51:18 +0530

    Sunday try

--- src/ai/tools.py
+++ src/ai/tools.py
@@ -29,6 +29,8 @@ def get_document(document_id:int, config:RunnableConfig):
     """
     configurable = config.get("metadata") or config.get("configurable")
     user_id = configurable.get("user_id")
+    if user_id is None:
+        raise Exception("Invalid requests for user")
     try:
         obj = Document.objects.get(id = document_id,owner_id= user_id ,active=True)
     except Document.DoesNotExist:
@@ -38,12 +40,40 @@ def get_document(document_id:int, config:RunnableConfig):
     response_data = []
     response_data.append({
             "id": obj.id,
-            "title": obj.title
+            "title": obj.title,
+            "content": obj.content,
+            "created_at" : obj.created_at
+        })
+    return response_data
+
+def create_document(title:str, content:str, config:RunnableConfig):
+    """
+    Create a new document based on the argument
+    title: string amx charaters of 120
+    content: long form text in many paragraphs or pages
+    """
+    configurable = config.get("metadata") or config.get("configurable")
+    user_id = configurable.get("user_id")
+    if user_id is None:
+        raise Exception("Invalid requests for user")
+    try:
+        obj = Document.objects.create(title=title, content=content,owner_id= user_id ,active=True)
+    except Document.DoesNotExist:
+        raise Exception("Document not found, try again")
+    except:
+        raise Exception("Invalid Request for Document, try again")
+    response_data = []
+    response_data.append({
+            "id": obj.id,
+            "title": obj.title,
+            "content": obj.content,
+            "created_at" : obj.created_at
         })
     return response_data
 
 
 document_tools = [
+    create_document,
     list_documents,
     get_document
 ]
